502ab104246e884d3eb22be95a9b4a586cb6fb9e
bexruzbek2024 <bexruzbaxtiyorov989@gmail.com>
2024-08-29 11:16:56 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+let a = +prompt("birinchi sonni kiriting")
+let a = prompt("qiymat kiriting")
+let c = +prompt("ikinchi sonni kiriting")
+
+if (b == "+"){
+    alert
+}
+  
+else if (b == "-"){
+    alert(a-c)
+}
+
+else if (b == "*"){
+    alert(a*c)
+}
+
+else if (b == "/"){
+    alert(a/c)
+}
+
+
+else if (b =="%"){
+    alert(a*c / 100);
+}
+
+else{
+    alert("notogri qiymat kirittingiz")
+}
